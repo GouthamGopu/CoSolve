34d57cd98a6630391e9dd1707039c89f300b5a7c
GouthamGopu <gopugoutham3@gmail.com>
2024-11-15 19:00:11 +0530

    created message controllers,Routes ans Set up frontend

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js"
 import postRoute from "./routes/post.route.js"
+import messageRoute from "./routes/message.route.js"
 dotenv.config({});
 
 const app = express();
@@ -31,6 +32,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use("/api/v1/user",userRoute);
 app.use("/api/v1/post",postRoute);
+app.use("/api/v1/message",messageRoute);
 
 app.listen(PORT,()=>{
   connectDB();
